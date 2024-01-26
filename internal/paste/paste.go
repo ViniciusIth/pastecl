8809574b8ffc35ee8aaf8c7c58a195cf99909c80
ViniciusIth <vini.ithalo@hotmail.com>
2024-01-26 00:40:08 -0300

    feat: added the file upload method

--- internal/paste/paste.go
+++ internal/paste/paste.go
@@ -1,57 +1,100 @@
 package paste
 
 import (
-	"pastecl/internal/database"
-	"time"
+    "fmt"
+    "io"
+    "mime/multipart"
+    "os"
+    "pastecl/internal/database"
+    "time"
 
-	"github.com/google/uuid"
+    "github.com/google/uuid"
 )
 
 type Paste struct {
-	UUID       string `json:"uuid"`
-	Title      string `json:"title"`
-	CreatedAt  int64  `json:"created_at"`
-	ExpiresAt  int64  `json:"expires_at"`
-	Visibility bool   `json:"visibility"`
-	ControlKey string `json:"control_key"`
-	OwnerId    string `json:"ownser_id"`
+    UUID        string                `json:"uuid"`
+    Title       string                `json:"title"`
+    CreatedAt   int64                 `json:"created_at"`
+    ExpiresAt   int64                 `json:"expires_at"`
+    Visibility  bool                  `json:"visibility"`
+    ControlKey  string                `json:"control_key"`
+    OwnerId     string                `json:"ownser_id"`
+    FileContent *multipart.FileHeader `json:"file"`
 }
 
-func CreateAnonPaste(title string, expires_at int64, visibility bool) (*Paste, error) {
-	pasteID, err := uuid.NewV7()
-	if err != nil {
-		return nil, err
-	}
+func CreateAnonPaste(title string, expires_at int64, visibility bool, file *multipart.FileHeader) (*Paste, error) {
+    pasteID, err := uuid.NewV7()
+    if err != nil {
+        return nil, err
+    }
 
     if expires_at == 0 {
         expires_at = time.Now().AddDate(0, 6, 0).Unix()
     }
 
-	// Just for tests, use a better thing in the future
-	controlKey, err := uuid.NewV7()
+    // Just for tests, use a better thing in the future
+    controlKey, err := uuid.NewV7()
 
-	newPaste := Paste{
-		UUID:       pasteID.String(),
-		Title:      title,
-		CreatedAt:  time.Now().Unix(),
-		ExpiresAt:  expires_at,
-		Visibility: visibility,
-		ControlKey: controlKey.String(),
-	}
+    newPaste := Paste{
+        UUID:        pasteID.String(),
+        Title:       title,
+        CreatedAt:   time.Now().Unix(),
+        ExpiresAt:   expires_at,
+        Visibility:  visibility,
+        ControlKey:  controlKey.String(),
+        FileContent: file,
+    }
 
-	return &newPaste, nil
+    return &newPaste, nil
+}
+
+func getPasteById(id string) (*Paste, error) {
+    var savedPaste Paste
+
+    sqlStatement := `SELECT id, title, createdat, expiresat, visibility, ownerid FROM paste WHERE id = ?`
+    row := database.Access.QueryRow(sqlStatement, id)
+
+    err := row.Scan(&savedPaste.UUID, &savedPaste.Title, &savedPaste.CreatedAt, &savedPaste.ExpiresAt, &savedPaste.Visibility, &savedPaste.OwnerId)
+    if err != nil {
+        return nil, err
+    }
+
+    return &savedPaste, nil
 }
 
 func (pst *Paste) SaveToDB() error {
-	sqlStatement := `INSERT INTO paste 
+    sqlStatement := `INSERT INTO paste 
     (id, title, createdat, expiresat, visibility, controlkey, ownerid)
     VALUES (?, ?, ?, ?, ?, ?, ?);`
-	_, err := database.Access.Exec(sqlStatement,
-		pst.UUID, pst.Title, pst.CreatedAt, pst.ExpiresAt, pst.Visibility, pst.ControlKey, pst.OwnerId)
+    _, err := database.Access.Exec(sqlStatement,
+        pst.UUID, pst.Title, pst.CreatedAt, pst.ExpiresAt, pst.Visibility, pst.ControlKey, pst.OwnerId)
 
-	if err != nil {
-		return err
-	}
+    if err != nil {
+        return err
+    }
 
-	return nil
+    return nil
+}
+
+func (pst *Paste) SaveToFile() error {
+    file, err := pst.FileContent.Open()
+    if err != nil {
+        return err
+    }
+
+    defer file.Close()
+
+    saveFile, err := os.OpenFile(fmt.Sprintf("./data/%s.txt", pst.UUID), os.O_WRONLY|os.O_CREATE, 0666)
+    if err != nil {
+        return err
+    }
+
+    defer saveFile.Close()
+
+    _, err = io.Copy(saveFile, file)
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
--- internal/paste/service.go
+++ internal/paste/service.go
@@ -4,16 +4,34 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 var newPasteHandler = func(w http.ResponseWriter, r *http.Request) {
-	var pasteStructure Paste
-	json.NewDecoder(r.Body).Decode(&pasteStructure)
+    err := r.ParseMultipartForm(2 << 18) // 512 kb, I think
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	newPaste, err := CreateAnonPaste(pasteStructure.Title, pasteStructure.ExpiresAt, pasteStructure.Visibility)
+    _, header, err := r.FormFile("content")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    expires, _ := strconv.ParseInt(r.FormValue("expire_at"), 10, 64)
+    visibility, _ := strconv.ParseBool(r.FormValue("visibility"))
+
+	newPaste, err := CreateAnonPaste(r.FormValue("title"), expires, visibility, header)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+    err = newPaste.SaveToFile()
+    if err != nil {
+        log.Fatal(err)
+    }
 
 	newPaste.SaveToDB()
 
@@ -24,4 +42,17 @@ var newPasteHandler = func(w http.ResponseWriter, r *http.Request) {
 
     w.Write(jsonResult)
     w.WriteHeader(http.StatusCreated)
+}
+
+var getPasteHandler = func (w http.ResponseWriter, r *http.Request) {
+    pasteId := chi.URLParam(r, "id")
+    
+    savedPaste, err := getPasteById(pasteId)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        log.Fatal(err)
+    }
+
+    jsonResult, err := json.Marshal(savedPaste)
+    w.Write(jsonResult)
 }
